0aec08dd1bbbf4a16d672e867b0adcfb698f47c9
wazimuhb <hesbonosoro1@gmail.com>
2022-03-13 06:25:30 +0300

    credentials template

---
+++ src/credentials.config.template.js
@@ -0,0 +1,5 @@
+// !!DO NOT COMMIT credentials.config.js FILE TO GITHUB!!
+module.exports = {
+	GITHUB_TOKEN: 'PUT YOUR GITHUB ACCESS TOKEN HERE',
+	GITHUB_API_URL: 'https://api.github.com/graphql',
+};
